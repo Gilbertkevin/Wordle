1dfc9c68433c5b9fad072205025f31f556b4e25a
Kevin Gilbert <kgilbert.og@gmail.com>
2022-03-23 17:31:49 -0500

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Kevin Gilbert on 3/23/22.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
